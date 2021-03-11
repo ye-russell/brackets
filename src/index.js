6648703af35f611bf7ce6aea643d399f189083b5
ye-russell <bekberata@gmail.com>
2021-03-11 13:35:04 +0600

    feat: complete brackets tack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+//check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]) // -> true
+//check('[(])', [['(', ')'], ['[', ']']]) // -> false
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var newStr = str; //'([{}])'
+  for (let i = 0; i < str.length / 2; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) { //[['(', ')'], ['[', ']'], ['{', '}']]
+      let newBrackets = [];
+      newBrackets[j] = bracketsConfig[j].join(''); //[['()'], ['[]'], ['{}']]
+      newStr = newStr.replace(newBrackets[j], '') //'([{}])' => [['()'], ['[]'], ['{}']]
+    }
+  }
+  return newStr == 0 ? true : false
 }
